47a47df6507e945f06bd1c6b7c53914846ebdf18
lamChuanJiang <lamchuanjiang@gmail.com>
2016-03-12 12:48:55 +0800

    testing

--- autocmt.bat
+++ autocmt.bat
@@ -1,14 +1,18 @@
-:: execute this bat script in dest remote git repo dir
+:: !!! Execute this bat script in dest remote git repo dir whatever relative or absolute path.
+
+:: After you add an DIY alias for remote git repo you can set it an a variable like repo below
+:: `git remote add ife git@github.com:lamChuanJiang/ife.git`
+
 @set repo=ife
+
 @set branch=dev
 
 @git checkout %branch%
 @git pull %repo% %branch%
 @git add -A
 
-@set /p comments=enter coments that make sense for this commit:
+@set /p comments=Enter coments that make sense for this commit op : 
 @git commit -m "%comments%"
-@git merge
 @git push %repo% %branch%
 
 @pause
